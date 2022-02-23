5b79577157aa553f68bd1ce17aa1dea57e7f2a03
DaniRamos28 <91649967+DaniRamos28@users.noreply.github.com>
2022-02-23 14:16:33 +0800

    Update urls.py

--- Ourproject/Labapp/urls.py
+++ Ourproject/Labapp/urls.py
@@ -1,13 +1,20 @@
+from os import name
 from django.urls import path
-
+from django.conf import settings
 from . import views
 
 urlpatterns = [ 
-    path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('reg/', views.reg, name='reg'),
     path('homepage/', views.homepage, name='homepage'),
     path('software/', views.software, name='software'),
     path('softwaredata/', views.softwaredata, name='softwaredata'),
     path('hardware/', views.hardware, name='hardware'),
+    path('equipmentdevice/', views.EquipmentDevice, name='EquipmentDevice'),
+    path('equipmentdevice1/', views.equipmentdevice1, name='equipmentdevice1'),
+    path('performrequest/', views.performrequest, name='performrequest'),
+    path('records/', views.records, name='records'),
+    path('delete/<int:Pcnum>', views.delete, name='delete'),
     
 ]
